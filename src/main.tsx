52d5f26b5b23674b9baed1c3aabf2ea0301237fa
Veridiana Lucena <veridianalucena@gmail.com>
2025-02-17 10:46:07 -0300

    refactor: Reset Main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import './components/global.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+
   </StrictMode>,
 )
