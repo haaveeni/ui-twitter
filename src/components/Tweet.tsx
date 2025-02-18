6f9e3929cf66fdc8ea0b26f3c92d6fdf07c73199
Veridiana Lucena <veridianalucena@gmail.com>
2025-02-18 10:19:09 -0300

    feat: import css, icons from phosphor and create a tweet

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,7 +1,31 @@
+import './tweet.css'
+import { ChatCircle } from 'phosphor-react'
+
 export function Tweet() {
   return (
     <div className="tweet">
+      <img src="https://github.com/haaveeni.png" alt="Veridiana Lucena" />
+      <div className="tweet-content">
+        <div className="tweet-content-header">
+          <strong>Veridiana Lucena</strong>
+          <span>@haaveeni</span>
+        </div>
+        <p>
+          Acabei de migrar um projeto React GIGANTE de create-react-app para Vite e os resultados foram:<br />
+          <br />
+          ✅ npm start: De 32s para 400ms (sim, demorava 30s)<br />
+          ✅ npm build: De 120s para 22s<br />
 
+          Além disso, troquei do Yarn para o PNPM e o install das deps mudou de 24s para 8s 🔥
+        </p>
+
+        <div className="content-footer">
+          <button>
+            <ChatCircle style={{ color: '#fff' }} />
+          </button>
+
+        </div>
+      </div>
     </div>
   )
 }
