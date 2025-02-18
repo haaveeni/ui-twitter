100e7df9b2c295432b22473bdbfc9ae0b8413066
Veridiana Lucena <veridianalucena@gmail.com>
2025-02-17 21:01:08 -0300

    feat: add imports of icons from assets and create aside menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,47 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './components/global.css'
+import './global.css'
 
-createRoot(document.getElementById('root')!).render(
+import logo from './assets/x-logo.svg'
+import home from './assets/home-logo.svg'
+import explore from './assets/explore-logo.svg'
+import notifications from './assets/notifications-logo.svg'
+import messages from './assets/messages-logo.svg'
+import bookmarks from './assets/bookmarks-logo.svg'
+import lists from './assets/lists-logo.svg'
+import profile from './assets/profile-logo.svg'
+import more from './assets/more-logo.svg'
+import sparkle from './assets/sparkle-logo.svg'
+
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
+    <div className="layout">
+      <div className="sidebar">
+        <img className='logo' src={logo} alt="logo" />
+
+        <nav className="main-navigation">
+          <a href=""><img className='logo' src={home} alt="home-logo" />Home</a>
+          <a href=""><img className='logo' src={explore} alt="explore-logo" />Explore</a>
+          <a href=""><img className='logo' src={notifications} alt="notifications-logo" />Notifications</a>
+          <a href=""><img className='logo' src={messages} alt="messages-logo" />Messages</a>
+          <a href=""><img className='logo' src={bookmarks} alt="bookmarks-logo" />Bookmarks</a>
+          <a href=""><img className='logo' src={lists} alt="lists-logo" />Lists</a>
+          <a href=""><img className='logo' src={profile} alt="profile-logo" />Profile</a>
+          <a href=""><img className='logo' src={more} alt="more-logo" />More</a>
+        </nav>
+
+        <button className="new-tweet">Post</button>
+      </div>
+      <div className="content">
+        <main className="timeline">
+          <div className="timeline-header">
+            Home
+            <a href=""><img src={sparkle} alt="" /></a>
+          </div>
+        </main>
+      </div>
+    </div>
 
   </StrictMode>,
 )
