5979f94b3551178845ce300e3dad89554155c9be
Veridiana Lucena <veridianalucena@gmail.com>
2025-02-18 22:38:07 -0300

    feat: add status route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Status } from "./pages/Status";
 import { Timeline } from "./pages/Timeline";
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Timeline />
-  }
-]) 
+  },
+  {
+    path: '/status',
+    element: <Status />
+  }]) 
