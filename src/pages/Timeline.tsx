1a27b286544fa8d581063db854d7dcf630c0d791
Veridiana Lucena <veridianalucena@gmail.com>
2025-02-19 08:16:44 -0300

    feat: Add map,  useState, EventForm, onKeySubmit and onSubmit methods

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,19 +1,49 @@
+import { FormEvent, KeyboardEvent, useState } from 'react'
 import { Header } from '../components/Header'
 import { Separator } from '../components/Separator'
-
 import { Tweet } from '../components/Tweet'
 
 import './timeline.css'
 
 export function Timeline() {
+  const [newTweet, setNewTweet] = useState('')
+  const [tweets, setTweets] = useState([
+    'Meu primeiro tweet',
+    'Bom dia, pessoal! :)',
+    'Testando...'
+  ])
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault()
+
+    setTweets([...tweets, newTweet])
+    setNewTweet('')
+
+  }
+
+  function handleHotKeySubmit(event: KeyboardEvent) {
+    if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) {
+      setTweets([...tweets, newTweet])
+      setNewTweet('')
+    }
+  }
+
   return (
     <main className="timeline">
       <Header title="Home" />
 
-      <form className='new-tweet-form'>
+      <form onSubmit={createNewTweet} className='new-tweet-form'>
         <label htmlFor="tweet">
           <img src="https://github.com/haaveeni.png" alt="Veridiana Lucena" />
-          <textarea id="tweet" placeholder="What's happening?" />
+          <textarea
+            id="tweet"
+            placeholder="What's happening?"
+            value={newTweet}
+            onKeyDown={handleHotKeySubmit}
+            onChange={(event) => {
+              setNewTweet(event.target.value)
+            }}
+          />
         </label>
 
         <button type='submit'>Tweet</button>
@@ -21,10 +51,9 @@ export function Timeline() {
 
       <Separator />
 
-      <Tweet content='tweet 1' />
-      <Tweet content='tweet 2' />
-      <Tweet content='tweet 3' />
-      <Tweet content='tweet 4' />
+      {tweets.map(tweet => {
+        return <Tweet key={tweet} content={tweet} />
+      })}
     </main>
   )
 }
