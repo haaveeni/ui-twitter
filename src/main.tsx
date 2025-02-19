bf844c4ab0fcdc77876d08eda2081998988c9dab
Veridiana Lucena <veridianalucena@gmail.com>
2025-02-19 06:56:15 -0300

    refactor: move layout and content to Default layout component

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './global.css'
-
-import { Sidebar } from './components/Sidebar'
+import { Default } from './layouts/Default'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <div className="layout">
-      <Sidebar />
-      <div className="content">
-
-        <RouterProvider router={router} />
-
-      </div>
-    </div>
-
+    <RouterProvider router={router} />
   </StrictMode>,
 )
